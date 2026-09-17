6e758557e5cf220a9588b3865374d27b1785fe19
Viergi <viergiansoka@gmail.com>
2026-09-17 21:21:38 +0700

    feat: change link CV

--- app/page.jsx
+++ app/page.jsx
@@ -29,7 +29,7 @@ const Home = () => {
             </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <a
-                href="https://drive.google.com/uc?export=download&id=1C8hUIIHXj-FOBP0OaH3LZPrcw9lti1Fi"
+                href="https://drive.google.com/uc?export=download&id=1nIjsnag-UEVYy3E8JInJI5Mytn3OIVmK"
                 download
               >
                 <Button
